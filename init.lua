c8b83c745b283e1bb5bceb303a146c9b0a34720c
Nao <nao.m-lanao@epitech.eu>
2025-07-23 16:13:48 +0400

    Feat: LSP part 1

--- init.lua
+++ init.lua
@@ -2,3 +2,7 @@ require("naovim.start")
 require("config.lazy")
 require("naovim.setup")
 require("naovim.conf")
+
+require('mini.snippets').setup({})
+require('mini.completion').setup({})
+vim.lsp.enable({'rust_analyzer'})
--- lua/naovim/conf.lua
+++ lua/naovim/conf.lua
@@ -16,3 +16,12 @@ vim.keymap.set("n", "<leader>m", function() harpoon.ui:toggle_quick_menu(harpoon
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<C-j>", function() harpoon:list():prev() end)
 vim.keymap.set("n", "<C-k>", function() harpoon:list():next() end)
+
+-- LSP
+vim.lsp.config('luals', {
+  cmd = {'lua-language-server'},
+  filetypes = {'lua'},
+  root_markers = {'.luarc.json', '.luarc.jsonc'},
+})
+
+vim.lsp.enable('luals')
