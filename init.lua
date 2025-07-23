169e8ef6374fa035538cdbcb1851b52ebd5e573f
Nao <nao.m-lanao@epitech.eu>
2025-07-23 16:53:46 +0400

    Feat: error and warnings added

--- init.lua
+++ init.lua
@@ -5,4 +5,12 @@ require("naovim.conf")
 
 require('mini.snippets').setup({})
 require('mini.completion').setup({})
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
 vim.lsp.enable({'rust_analyzer'})
